7abe072bad0cb78c68f491bd794b7895394c3311
Rodrigo Lopes <rjr89000@gmail.com>
2023-04-25 18:54:34 -0300

    img home

--- src/main/java/com/web/deploy/SpringWeb/repositorio/AdministradoresRepo.java
+++ src/main/java/com/web/deploy/SpringWeb/repositorio/AdministradoresRepo.java
@@ -1,10 +1,15 @@
 package com.web.deploy.SpringWeb.repositorio;
 
+import org.springframework.data.jpa.repository.Query;
 // import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.web.deploy.SpringWeb.models.Administrador;
 
 public interface AdministradoresRepo extends CrudRepository<Administrador, Integer> {
+  @Query(value = "select CASE WHEN count(1) > 0 THEN 'true' ELSE 'false' END  from administradores where id = :id", nativeQuery = true)
+  public boolean exist(int id);
 
+  @Query(value = "select * from administradores where email = :email and senha = :senha", nativeQuery = true)
+  public Administrador Login(String email, String senha);
 }
